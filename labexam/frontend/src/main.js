62272457e71c49404b4870a9fa11e53e0d447754
amularloui <amularjohnloui61@gmail.com>
2025-05-12 16:53:18 +0800

    change file name

--- labexam/frontend/src/main.js
+++ labexam/frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 
 // ✅ Import Bootstrap CSS and JS
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js' // <-- this line is required!
+import 'bootstrap/dist/js/bootstrap.bundle.min' // <-- this line is required!
 
 // Initialize API services
 import './services/api'
